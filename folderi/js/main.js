1b48dc7fc90080b0741ea8a7ffa68484d91bdf24
anriterrson <anriyestroll@gmail.com>
2025-10-06 16:18:22 +0400

    Add files via upload

---
+++ folderi/js/main.js
@@ -0,0 +1,116 @@
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+    });
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+const navLink = document.querySelectorAll('.nav_link');
+const linkAction = () => navMenu.classList.remove('show-menu');
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+const scrollHeader = () =>{
+    const header = document.getElementById('header');
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header');
+};
+window.addEventListener('scroll', scrollHeader);
+
+
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up');
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                         : scrollUp.classList.remove('show-scroll');
+};
+window.addEventListener('scroll', scrollUp);
+
+
+const sections = document.querySelectorAll('section[id]');
+const scrollActive = () =>{
+    const scrollY = window.pageYOffset;
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']');
+        if(sectionsClass){
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                sectionsClass.classList.add('active-link');
+            }else{
+                sectionsClass.classList.remove('active-link');
+            } 
+        } 
+    });
+};
+window.addEventListener('scroll', scrollActive);
+
+/*Dark Mode*/ 
+const themeButton = document.getElementById('theme-button'),
+      darkTheme = 'dark-theme',
+      iconTheme = 'ri-sun-line';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+function updateAuthLink() {
+    // ამოწმებს თუ იყო შესული ექუნთზე
+    const isLoggedIn = sessionStorage.getItem('isLoggedIn') === 'true';
+
+    const authLinkElement = document.getElementById('auth-link');
+    const authTextElement = document.getElementById('auth-text');
+    const userIcon = document.getElementById('user-icon');
+    const profileImg = document.getElementById('profile-img');
+
+    if (!authLinkElement || !authTextElement) {
+        return;
+    }
+
+    if (isLoggedIn) {
+        //USER შევიდა
+        authLinkElement.href = "index.html"; 
+        
+        //profile image
+        userIcon.style.display = 'none';
+        profileImg.style.display = 'inline-block';
+        const username = sessionStorage.getItem('username') || 'Profile';
+        authTextElement.textContent = username;
+
+    } else {
+    
+        authLinkElement.href = "login.html";
+    
+        userIcon.style.display = 'inline-block';
+        profileImg.style.display = 'none';
+        
+        authTextElement.textContent = "Login";
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', updateAuthLink);
